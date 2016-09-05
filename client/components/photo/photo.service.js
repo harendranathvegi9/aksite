69a97a33fd71ca62836a535d5de3b2042befb951
Andrew Koroluk <koroluka@gmail.com>
2016-09-05 01:10:51 -0400

    refactor(client:photoService): ng2

--- client/components/photo/photo.service.js
+++ client/components/photo/photo.service.js
@@ -1,9 +1,56 @@
 'use strict';
 import angular from 'angular';
+import {Injectable} from '@angular/core';
+import {Http, Response} from '@angular/http';
+import {AuthHttp} from 'angular2-jwt';
+import 'rxjs/add/operator/toPromise';
+import {upgradeAdapter} from '../../app/upgrade_adapter';
 
-angular.module('aksiteApp')
-    .factory('Photo', function Auth($resource) {
-        return $resource('/api/photos/:id/:controller', {
-            id: '@_id'
-        });
-    });
+@Injectable()
+export class PhotoService {
+    static parameters = [Http, AuthHttp];
+    constructor(http, authHttp) {
+        this.http = http;
+        this.authHttp = authHttp;
+    }
+
+    handleError(err) {
+        throw err;
+    }
+
+    query() {
+        return this.http.get('/api/photos/')
+            .toPromise()
+            .then(extractData)
+            .catch(this.handleError);
+    }
+    get(photo: {id: 'string'}) {
+        return this.http.get(`/api/photos/${photo.id}`)
+            .toPromise()
+            .then(extractData)
+            .catch(this.handleError);
+    }
+    create(photo) {
+        return this.http.post('/api/photos/', photo)
+            .toPromise()
+            .then(extractData)
+            .catch(this.handleError);
+    }
+    remove(photo: {id: 'string'}) {
+        return this.authHttp.delete(`/api/photos/${photo.id}`)
+            .toPromise()
+            .then(extractData)
+            .catch(this.handleError);
+    }
+}
+
+function extractData(res: Response) {
+    if(!res.text()) return {};
+    return res.json() || { };
+}
+
+upgradeAdapter.addProvider(PhotoService);
+
+export default angular.module('factories.Photo', [])
+    .factory('Photo', upgradeAdapter.downgradeNg2Provider(PhotoService))
+    .name;
