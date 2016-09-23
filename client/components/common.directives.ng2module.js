8ba4cdfb714446eb85a6b671ea3ee98069bf58ee
Andrew Koroluk <koroluka@gmail.com>
2016-09-22 21:36:18 -0400

    feat(client:directives): add preloader to directives module

--- client/components/common.directives.ng2module.js
+++ client/components/common.directives.ng2module.js
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { CollapseModule } from 'ng2-bootstrap/components/collapse';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { PreloaderComponent } from './preloader/preloader.component';
 
 @NgModule({
     imports: [CollapseModule, CommonModule],
     declarations: [
         NavbarComponent,
-        FooterComponent
+        FooterComponent,
+        PreloaderComponent,
     ]
 })
 export class DirectivesModule {}
--- client/components/preloader/preloader.component.js
+++ client/components/preloader/preloader.component.js
@@ -1,13 +1,7 @@
-import angular from 'angular';
 import {Component} from '@angular/core';
-import {upgradeAdapter} from '../../app/upgrade_adapter';
 
 @Component({
     selector: 'preloader',
     template: require('./preloader.html')
 })
 export class PreloaderComponent {}
-
-export default angular.module('directives.preloader', [])
-    .directive('preloader', upgradeAdapter.downgradeNg2Component(PreloaderComponent))
-    .name;
