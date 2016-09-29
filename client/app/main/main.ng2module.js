c7c7127bc967c5497a71dfa503c6408be1126881
Andrew Koroluk <koroluka@gmail.com>
2016-09-28 20:25:10 -0400

    refactor(client:socketService): ng2

--- client/app/main/main.ng2module.js
+++ client/app/main/main.ng2module.js
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { MainComponent } from './main.component';
+import { SocketService } from '../../components/socket/socket.service';
 
 @NgModule({
     declarations: [
         MainComponent
+    ],
+    providers: [
+        SocketService
     ]
 })
 export class MainModule {}
--- client/components/socket/socket.service.js
+++ client/components/socket/socket.service.js
@@ -1,69 +1,67 @@
 'use strict';
-import angular from 'angular';
+import { Injectable } from '@angular/core';
 import {noop, find, remove} from 'lodash-es';
 import io from 'socket.io-client';
 
-export function SocketService() {
-    'ngInject';
+@Injectable()
+export class SocketService {
+    socket;
 
-    // socket.io now auto-configures its connection when we ommit a connection url
-    var socket = io('', {
-        // Send auth token on connection, you will need to DI the Auth service above
-        // 'query': 'token=' + Auth.getToken()
-    });
+    constructor() {
+        this.socket = io('', {
+            // Send auth token on connection, you will need to DI the Auth service above
+            // 'query': 'token=' + Auth.getToken()
+        });
+    }
 
-    return {
-        socket,
+    /**
+     * Register listeners to sync an array with updates on a model
+     *
+     * Takes the array we want to sync, the model name that socket updates are sent from,
+     * and an optional callback function after new items are updated.
+     *
+     * @param {String} modelName
+     * @param {Array} array
+     * @param {Function} cb
+     */
+    syncUpdates(modelName, array, cb = noop) {
+        /**
+         * Syncs item creation/updates on 'model:save'
+         */
+        this.socket.on(`${modelName}:save`, function(item) {
+            var oldItem = find(array, {_id: item._id});
+            var index = array.indexOf(oldItem);
+            var event = 'created';
+
+            // replace oldItem if it exists
+            // otherwise just add item to the collection
+            if(oldItem) {
+                array.splice(index, 1, item);
+                event = 'updated';
+            } else {
+                array.push(item);
+            }
+
+            cb(event, item, array);
+        });
 
         /**
-         * Register listeners to sync an array with updates on a model
-         *
-         * Takes the array we want to sync, the model name that socket updates are sent from,
-         * and an optional callback function after new items are updated.
-         *
-         * @param {String} modelName
-         * @param {Array} array
-         * @param {Function} cb
+         * Syncs removed items on 'model:remove'
          */
-        syncUpdates(modelName, array, cb = noop) {
-            /**
-             * Syncs item creation/updates on 'model:save'
-             */
-            socket.on(`${modelName}:save`, function(item) {
-                var oldItem = find(array, {_id: item._id});
-                var index = array.indexOf(oldItem);
-                var event = 'created';
+        this.socket.on(`${modelName}:remove`, function(item) {
+            var event = 'deleted';
+            remove(array, {_id: item._id});
+            cb(event, item, array);
+        });
+    }
 
-                // replace oldItem if it exists
-                // otherwise just add item to the collection
-                if(oldItem) {
-                    array.splice(index, 1, item);
-                    event = 'updated';
-                } else {
-                    array.push(item);
-                }
-
-                cb(event, item, array);
-            });
-
-            /**
-             * Syncs removed items on 'model:remove'
-             */
-            socket.on(`${modelName}:remove`, function(item) {
-                var event = 'deleted';
-                remove(array, {_id: item._id});
-                cb(event, item, array);
-            });
-        },
-
-        /**
-         * Removes listeners for a models updates on the socket
-         *
-         * @param modelName
-         */
-        unsyncUpdates(modelName) {
-            socket.removeAllListeners(`${modelName}:save`);
-            socket.removeAllListeners(`${modelName}:remove`);
-        }
-    };
+    /**
+     * Removes listeners for a models updates on the socket
+     *
+     * @param modelName
+     */
+    unsyncUpdates(modelName) {
+        this.socket.removeAllListeners(`${modelName}:save`);
+        this.socket.removeAllListeners(`${modelName}:remove`);
+    }
 }
