7fb37c50950b2afd56ce9dae6b6a6d42bacd86ed
Andrew Koroluk <koroluka@gmail.com>
2017-02-26 19:21:03 -0500

    chore(package): update angular material

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -39,7 +39,7 @@ class RavenErrorHandler implements ErrorHandler {
     imports: [
         BrowserModule,
         HttpModule,
-        MaterialModule.forRoot(),
+        MaterialModule,
         UIRouterModule.forRoot({useHash: true}),
         MainModule,
         DirectivesModule,
